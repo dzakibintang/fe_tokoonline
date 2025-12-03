707919ce96f83cf20832bed4f610e52b164a8e67
dzaki bintang hardiansyah <dzakibintanghardiansyah@dzakis-MacBook.local>
2025-12-03 15:30:52 +0700

    fix route

--- app/login/page.js
+++ app/login/page.js
@@ -14,7 +14,7 @@ export default function LoginPage() {
     setError('');
 
     try {
-      const res = await fetch('betokoonline-production-fbdf.up.railway.app/api/auth/login', {
+      const res = await fetch('${process.env.NEXT_PUBLIC_API_URL}/api/auth/login', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify({
